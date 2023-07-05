6c489b9e92489040b694774bacc76f4551044580
Guillaume Wells <wellsguillaume@gmail.com>
2023-07-05 22:10:08 +0200

    feat(UI): some colors

--- camera.php
+++ camera.php
@@ -9,8 +9,8 @@ require_once '/srv/http/vendor/autoload.php';
 
 $color = new Color(['red' => 255, 'green' => 0, 'blue' => 0]);
 
-const IMAGE_WIDTH = 1600;
-const IMAGE_HEIGHT = 1300;
+const IMAGE_WIDTH = 890;
+const IMAGE_HEIGHT = 890;
 
 $corner1 = new Vertex( array( 'x' => 1, 'y' => -1, 'z' => -5, 'color' => $color ) );
 $corner2 = new Vertex( array( 'x' => 1, 'y' => -1, 'z' => -3, 'color' => $color ) );
@@ -61,11 +61,13 @@ if (isset($_POST['z-rotation'])) {
     $_SESSION['from'] = $M->transformVertex($_SESSION['from']);
 }
 
+
 $to = new Vertex( array( 'x' => 1, 'y' => 1, 'z' => -5, 'color' => $color ) );
 $viewMatrix = new Matrix( array( 'preset' => Matrix::CAMERATOWORLD , 'from' => $_SESSION['from'], 'to' => $to));
-//$inv = new Matrix( array( 'preset' => Matrix::INVERSE , 'matrix' => $viewMatrix));
 
 $projectedCorners = [];
+/* $camera = $viewMatrix->transformVertex($_SESSION['from']); */
+/* $camera = Vertex::projectPoint3($camera); */
 foreach ($corners as &$corner) 
 {
     $corner = $viewMatrix->transformVertex($corner);
@@ -74,6 +76,10 @@ foreach ($corners as &$corner)
 
 $image = imagecreatetruecolor(IMAGE_WIDTH, IMAGE_HEIGHT);
 $col_poly = imagecolorallocate($image, $color->red, $color->green, $color->blue);
+$downColor = new Color(['red' => 102, 'green' => 252, 'blue' => 102]);
+$downColor = imagecolorallocate($image, $downColor->red, $downColor->green, $downColor->blue);
+$topColor = new Color(['red' => 252, 'green' => 186, 'blue' => 3]);
+$topColor = imagecolorallocate($image, $topColor->red, $topColor->green, $topColor->blue);
 
 imagepolygon($image, [
     $projectedCorners[5]->getX(), $projectedCorners[5]->getY(),
@@ -89,12 +95,6 @@ imagepolygon($image, [
     $projectedCorners[7]->getX(), $projectedCorners[7]->getY(),
 ], 4, $col_poly);
 
-imagepolygon($image, [
-    $projectedCorners[5]->getX(), $projectedCorners[5]->getY(),
-    $projectedCorners[4]->getX(), $projectedCorners[4]->getY(),
-    $projectedCorners[6]->getX(), $projectedCorners[6]->getY(),
-    $projectedCorners[7]->getX(), $projectedCorners[7]->getY(),
-], 4, $col_poly);
 
 imagepolygon($image, [
     $projectedCorners[4]->getX(), $projectedCorners[4]->getY(),
@@ -102,14 +102,20 @@ imagepolygon($image, [
     $projectedCorners[2]->getX(), $projectedCorners[2]->getY(),
     $projectedCorners[6]->getX(), $projectedCorners[6]->getY(),
 ], 4, $col_poly);
+
+imagepolygon($image, [
+    $projectedCorners[5]->getX(), $projectedCorners[5]->getY(),
+    $projectedCorners[4]->getX(), $projectedCorners[4]->getY(),
+    $projectedCorners[6]->getX(), $projectedCorners[6]->getY(),
+    $projectedCorners[7]->getX(), $projectedCorners[7]->getY(),
+], 4, $topColor);
 
 imagepolygon($image, [
     $projectedCorners[0]->getX(), $projectedCorners[0]->getY(),
     $projectedCorners[1]->getX(), $projectedCorners[1]->getY(),
     $projectedCorners[3]->getX(), $projectedCorners[3]->getY(),
     $projectedCorners[2]->getX(), $projectedCorners[2]->getY(),
-], 4, $col_poly);
-
+], 4, $downColor);
 
 header('Content-type: image/png');
 
